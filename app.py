f4e989915af5b3936d3fa902a6da186544b8019f
haidizym <67679461+haidizym@users.noreply.github.com>
2024-05-13 14:19:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import torch
 import os
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
-base_path = './'  #模型下载的文件夹
+base_path = './final_model'  #模型下载的文件夹
 
 #os.system(f'git clone 模型地址.git {base_path}') #这里的模型地址就是你模型的下载地址，简单说就是前面创建模型时候git clone的同样代码
 #os.system(f'git@code.openxlab.org.cn:zhuyamei/final_model.git {base_path}')
